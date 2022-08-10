f0f22099e17df255cd980a4c04355f143b26b736
Daniel <73000732+MonnTS@users.noreply.github.com>
2022-08-10 23:12:59 +0200

    Refactored class; Added OnExit method;

--- DBRentalsApplication/App.xaml.cs
+++ DBRentalsApplication/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Configuration;
+using System.Windows;
 using DBRentalsApplication.Models;
 using DBRentalsApplication.Services;
 using DBRentalsApplication.Stores;
@@ -7,64 +8,65 @@ using DBRentalsApplication.Views;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using ConfigurationManager = System.Configuration.ConfigurationManager;
-using ILogger = Serilog.ILogger;
 
-namespace DBRentalsApplication
+namespace DBRentalsApplication;
+
+public partial class App : Application
 {
-    public partial class App : Application
-    {
-        private readonly ServiceProvider _serviceProvider;
-        private ILogger Logger { get; set; }
+    private readonly ServiceProvider _serviceProvider;
 
-        public App()
+    public App()
+    {
+        var services = new ServiceCollection();
+        ConfigureServices(services);
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    private ILogger Logger { get; set; }
+
+    private void ConfigureServices(IServiceCollection services)
+    {
+        Logger = new LoggerConfiguration()
+            .WriteTo.File("Logs\\DBRentals.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        Log.Debug("Logger configured");
+
+        services.AddDbContext<ProdContext>(options =>
         {
-            ServiceCollection services = new ServiceCollection();
-            ConfigureServices(services);
-            _serviceProvider = services.BuildServiceProvider();
-        }
-        
-        private void ConfigureServices(IServiceCollection services)
+            options.UseSqlServer(ConfigurationManager.ConnectionStrings["RentalDataBase"].ConnectionString);
+        });
+        services.AddTransient(x =>
         {
-            Logger = new LoggerConfiguration()
-                .WriteTo.File("Logs\\DBRentals.txt", rollingInterval: RollingInterval.Day)
-                .CreateLogger();
-            
-            Log.Debug("Logger configured");
-            
-            services.AddDbContext<ProdContext>(options =>
+            var navigationStore = x.GetRequiredService<NavigationStore>();
+            var mainWindow = new MainWindow
             {
-                options.UseSqlServer(ConfigurationManager.ConnectionStrings["RentalDataBase"].ConnectionString);
-            });
-            services.AddTransient(x =>
-            {
-                var navigationStore = x.GetRequiredService<NavigationStore>();
-                var mainWindow = new MainWindow
-                {
-                    DataContext = new MainViewModel(navigationStore)
-                };
-                return mainWindow;
-            });
-            services.AddTransient<DeleteRentalView>();
-            services.AddSingleton<DataBaseService>();
-            services.AddSingleton(x =>
-            {
-                var navigationStore = new NavigationStore();
-                var dbService = x.GetRequiredService<DataBaseService>();
-                navigationStore.CurrentViewModel = new RentalViewModel(dbService, navigationStore);
-                return navigationStore;
-            });
-            services.AddSingleton<CreateRentalViewModel>();
-            services.AddLogging(x =>
-            {
-                x.AddSerilog(Logger, dispose: true);
-            });
-        }
-      
-        private void OnStartup(object sender, StartupEventArgs e)
+                DataContext = new MainViewModel(navigationStore)
+            };
+            return mainWindow;
+        });
+        services.AddTransient<DeleteRentalView>();
+        services.AddSingleton<DataBaseService>();
+        services.AddSingleton(x =>
         {
-            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
-            mainWindow.Show();
-        }
+            var navigationStore = new NavigationStore();
+            var dbService = x.GetRequiredService<DataBaseService>();
+            navigationStore.CurrentViewModel = new RentalViewModel(dbService, navigationStore);
+            return navigationStore;
+        });
+        services.AddSingleton<CreateRentalViewModel>();
+        services.AddLogging(x => { x.AddSerilog(Logger, true); });
+    }
+
+    private void OnStartup(object sender, StartupEventArgs e)
+    {
+        var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+        mainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _serviceProvider.Dispose();
+        Log.Information("Application closed");
     }
 }
